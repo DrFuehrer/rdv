5e495d1424675ee458c71bba361509e87849a07a
jphanley <jphanley@ce171161-c33e-0410-912a-ab1c26d05d24>
2005-01-25 23:04:06 +0000

    Added hardware scaling to jpeg data viewers.

---
+++ trunk/src/org/nees/buffalo/rbnb/dataviewer/Version.java
@@ -0,0 +1,11 @@
+package org.nees.buffalo.rbnb.dataviewer;
+
+/**
+ * @author Jason P. Hanley
+ */
+public class Version {
+	public static final int major = 1;
+	public static final int minor = 0;
+	public static final int release = 2;
+	public static final String date = "20050125 1802"; 
+}
