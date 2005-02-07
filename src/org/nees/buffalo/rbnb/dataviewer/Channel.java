19fbf4a5224c9e44883c5633408c2873a7b9b343
jphanley <jphanley@ce171161-c33e-0410-912a-ab1c26d05d24>
2005-02-07 19:04:39 +0000

    Merge with units branch.

---
+++ src/org/nees/buffalo/rbnb/dataviewer/Channel.java
@@ -0,0 +1,44 @@
+/*
+ * Created on Feb 5, 2005
+ */
+package org.nees.buffalo.rbnb.dataviewer;
+
+/**
+ * @author Jason P. Hanley
+ */
+public class Channel {
+
+	String name;
+	String mimeType;
+	String unit;
+	
+	public Channel(String name) {
+		this(name, null, null);
+	}
+	
+	public Channel(String name, String mimeType, String unit) {
+		this.name = name;
+		this.mimeType = mimeType;
+		this.unit = unit;
+	}
+	
+	public String getName() {
+		return name.substring(name.lastIndexOf("/")+1);
+	}
+	
+	public String getFullName() {
+		return name;
+	}
+	
+	public String getParent() {
+		return name.substring(0, name.lastIndexOf("/"));
+	}
+	
+	public String getMimeType() {
+		return mimeType;
+	}
+	
+	public String getUnit() {
+		return unit;
+	}
+}
