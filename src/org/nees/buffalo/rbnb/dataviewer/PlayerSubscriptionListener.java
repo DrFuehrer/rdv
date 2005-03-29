efe5f54421eef1f3cead600221114d19cf8bfc35
jphanley <jphanley@ce171161-c33e-0410-912a-ab1c26d05d24>
2005-03-29 23:06:20 +0000

    Add javadoc.

--- src/org/nees/buffalo/rbnb/dataviewer/PlayerSubscriptionListener.java
+++ src/org/nees/buffalo/rbnb/dataviewer/PlayerSubscriptionListener.java
@@ -1,9 +1,26 @@
 package org.nees.buffalo.rbnb.dataviewer;
 
 /**
- * @author Jason P. Hanley
+ * A listener interface to notify listeners when a channel has
+ * been subscribed too or unsubscribed too in the player.
+ * 
+ * @author  Jason P. Hanley
+ * @since   1.0
  */
 public interface PlayerSubscriptionListener {
+	/**
+	 * Called when the player subscribes to the specified channel.
+	 * 
+	 * @param channelName  the channel subscribed too
+	 * @since              1.0
+	 */
 	public void channelSubscribed(String channelName);
+	
+	/**
+	 * Called when the player unsubscribes from the specified channel.
+	 * 
+	 * @param channelName  the channel unscubscribed from
+	 * @since              1.0
+	 */
 	public void channelUnsubscribed(String channelName);
 }
