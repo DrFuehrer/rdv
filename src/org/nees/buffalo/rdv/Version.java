7de41ea3310c61dd285dafd28e9685411ac16856
jphanley <jphanley@ce171161-c33e-0410-912a-ab1c26d05d24>
2005-06-20 17:00:10 +0000

    RDV 1.2.0_beta3

--- src/org/nees/buffalo/rdv/Version.java
+++ src/org/nees/buffalo/rdv/Version.java
@@ -34,7 +34,7 @@ public final class Version {
 	 *
 	 * @since  1.2
 	 */
-	public static final String build = "_beta2";
+	public static final String build = "_beta3";
 	
 	/**
 	 * This class can not be instantiated and it's constructor
