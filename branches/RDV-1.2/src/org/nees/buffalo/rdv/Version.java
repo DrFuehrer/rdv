1ea7daa40536449b1344625a8a259126c376feab
jphanley <jphanley@ce171161-c33e-0410-912a-ab1c26d05d24>
2005-06-21 15:54:37 +0000

    Branch for future RDV 1.2 fixes.

---
+++ branches/RDV-1.2/src/org/nees/buffalo/rdv/Version.java
@@ -0,0 +1,46 @@
+package org.nees.buffalo.rdv;
+
+/**
+ * A class to encapsulate the version of the application.
+ * The version is displayed in the format of major.minor.release.build.
+ * 
+ * @author  Jason P. Hanley
+ * @since   1.0
+ */
+public final class Version {
+	/**
+	 * The major version number.
+	 * 
+	 * @since  1.0
+	 */
+	public static final int major = 1;
+	
+	/**
+	 * The minor version number
+	 * 
+	 * @since  1.0 
+	 */
+	public static final int minor = 2;
+	
+	/**
+	 * The release number.
+	 * 
+	 * @since  1.0
+	 */
+	public static final int release = 0;
+	
+	/**
+	 * The build string.
+	 *
+	 * @since  1.2
+	 */
+	public static final String build = "";
+	
+	/**
+	 * This class can not be instantiated and it's constructor
+	 * always throws an exception.
+	 */
+	private Version() {
+		throw new UnsupportedOperationException("This class can not be instantiated.");
+	}
+}
