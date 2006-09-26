ce4df31d08e8819cb82cafefd3691803ceaf7822
jphanley <jphanley@ce171161-c33e-0410-912a-ab1c26d05d24>
2006-09-26 18:12:17 +0000

    Move old unit test out of source tree into test tree.

--- trunk/tests/org/nees/rbnb/DataTurbineTest.java
+++ trunk/tests/org/nees/rbnb/DataTurbineTest.java
@@ -1,36 +1,68 @@
+/*
+ * RDV
+ * Real-time Data Viewer
+ * http://it.nees.org/software/rdv/
+ * 
+ * Copyright (c) 2005-2006 University at Buffalo
+ * Copyright (c) 2005-2006 NEES Cyberinfrastructure Center
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ * $URL$
+ * $Revision$
+ * $Date$
+ * $Author$
+ */
+
 package org.nees.rbnb;
 /**
- * @author Lawrence J. Miller <ljmiller@sdsc.edu>
+
  * @author NEES Cyberinfrastructure Center (NEESit), San Diego Supercomputer Center
  * Please see copywrite information at the end of this file.
  * @since 060120
  */
-import com.rbnb.sapi.ChannelMap;
-import com.rbnb.sapi.Sink;
-import com.rbnb.sapi.SAPIException;
+import junit.framework.TestCase;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.nees.rbnb.DataTurbine;
 import org.nees.rbnb.marker.EventMarker;
 
-import junit.framework.TestCase;
-import junit.framework.TestResult;
+import com.rbnb.sapi.ChannelMap;
+import com.rbnb.sapi.SAPIException;
+import com.rbnb.sapi.Sink;
 
-/** A class that is a junit unit test to validate
-* the usage of
-* @see org.nees.rbnb.marker.NeesEvent.java
-* and the DataTurbine wrapper class
-* @see org.nees.rbnb.DataTurbine */
-public class TestDataTurbine extends TestCase {
+/**
+ * A unit test to validate the usage of the DataTurbine wrapper class.
+ * 
+ * @author Lawrence J. Miller
+ * @see org.nees.rbnb.DataTurbine
+ */
+public class DataTurbineTest extends TestCase {
   
-  static Log log = LogFactory.getLog (TestDataTurbine.class.getName ());
+  static Log log = LogFactory.getLog (DataTurbineTest.class.getName ());
   private DataTurbine turban;
-  private ChannelMap turbanCmap, cmap; 
+  private ChannelMap cmap; 
   private String dtName = "dev-neestpm.sdsc.edu";
   private Sink turbanSink;
   private String testString = "markerDTjunit";
   
-  public TestDataTurbine (String nme) {
+  public DataTurbineTest (String nme) {
     this.turban = new DataTurbine (testString);
     this.turban.setServerName (this.dtName);
     try  {
@@ -72,7 +104,6 @@ public class TestDataTurbine extends TestCase {
     // useful variable for initial unit test driven development
     boolean isMarkerChannel = false;
     try {
-      String[] inChannels = cmap.GetChannelList ();
       gotMap = turbanSink.Fetch (100, cmap);
       String[] gotChannels = gotMap.GetChannelList ();
       log.debug ("fetched " + gotChannels.length + " channels from the test DT");
@@ -88,38 +119,9 @@ public class TestDataTurbine extends TestCase {
     }
     
     int testIndex = gotMap.GetIndex (testString+ "/" + testString);
-    String [] gotArray = gotMap.GetDataAsString (testIndex);
     // indicates that some data was indeed fetched
     assertTrue (0 < testIndex);
     turbanSink.CloseRBNBConnection ();
    
   } // testDT ()
 } // class
-
- /** Copyright (c) 2005, Lawrence J. Miller and NEESit
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- *    * Redistributions of source code must retain the above copyright notice,
- * this list of conditions and the following disclaimer.
- *    * Redistributions in binary form must reproduce the above copyright
- * notice, this list of conditions and the following disclaimer in the 
- * documentation and/or other materials provided with the distribution.
- *   * Neither the name of the San Diego Supercomputer Center nor the names of
- * its contributors may be used to endorse or promote products derived from this
- * software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- */
