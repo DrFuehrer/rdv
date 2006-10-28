062471d3b269385f91b35fad104e59829de7b7c2
msoltani <msoltani@ce171161-c33e-0410-912a-ab1c26d05d24>
2006-10-28 00:04:30 +0000

    Add logger listener interface and manager classes

---
+++ src/org/nees/buffalo/rdv/rbnb/LogListener.java
@@ -0,0 +1,14 @@
+package org.nees.buffalo.rdv.rbnb;
+
+/**
+ * A listener interface to receive write to log message
+ *
+ */
+public interface LogListener {
+	
+	/**
+	 * method to write messages to log
+	 */
+	public void writeLog();
+
+}
