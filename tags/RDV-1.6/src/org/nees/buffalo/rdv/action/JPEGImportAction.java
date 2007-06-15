0853f7ed1e2e4a97ac922ff8aedd525a412d8977
fsayed <fsayed@ce171161-c33e-0410-912a-ab1c26d05d24>
2007-06-15 22:30:09 +0000

    Made a release tag for RDV 1.6

---
+++ tags/RDV-1.6/src/org/nees/buffalo/rdv/action/JPEGImportAction.java
@@ -0,0 +1,174 @@
+/*
+ * RDV
+ * Real-time Data Viewer
+ * http://it.nees.org/software/rdv/
+ * 
+ * Copyright (c) 2005-2007 University at Buffalo
+ * Copyright (c) 2005-2007 NEES Cyberinfrastructure Center
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ * $URL$
+ * $Revision$
+ * $Date$
+ * $Author$
+ */
+
+package org.nees.buffalo.rdv.action;
+
+import java.awt.event.ActionEvent;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.ParseException;
+
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+
+import org.nees.buffalo.rdv.data.JPEGFileDataSample;
+import org.nees.buffalo.rdv.data.JPEGFileCollectionReader;
+import org.nees.buffalo.rdv.rbnb.RBNBController;
+import org.nees.buffalo.rdv.rbnb.RBNBException;
+import org.nees.buffalo.rdv.rbnb.RBNBSource;
+import org.nees.buffalo.rdv.ui.ProgressWindow;
+
+/**
+ * A class to import a collection of JPEG images into an RBNB server. 
+ * 
+ * @author Jason P. Hanley
+ */
+public class JPEGImportAction extends DataViewerAction {
+  /** the window to show the progress of the import */
+  private ProgressWindow progressWindow;
+  
+  public JPEGImportAction() {
+    super("Import JPEG files",
+          "Import a folder that contains JPEG files");
+  }
+  
+  /**
+   * Prompts the user for the directory to import the JPEG images from and
+   * upload them to the RBNB server.
+   */
+  public void actionPerformed(ActionEvent ae) {
+    final File directory = getDirectory();
+    
+    if (directory == null) {
+      return;
+    }
+    
+    if (!directory.exists()) {
+      return;
+    }
+    
+    if (!directory.isDirectory()) {
+      return;
+    }
+    
+    progressWindow = new ProgressWindow("Importing data...");
+    progressWindow.setVisible(true);    
+    
+    new Thread() {
+      public void run() {        
+        try {
+          importDirectory(directory);
+        } catch (FileNotFoundException e) {
+          // TODO Auto-generated catch block
+          e.printStackTrace();
+        } catch (ParseException e) {
+          // TODO Auto-generated catch block
+          e.printStackTrace();
+        } catch (IOException e) {
+          // TODO Auto-generated catch block
+          e.printStackTrace();
+        } catch (RBNBException e) {
+          // TODO Auto-generated catch block
+          e.printStackTrace();
+        }
+        
+        progressWindow.dispose();
+        
+        RBNBController.getInstance().updateMetadata();
+        
+        JOptionPane.showMessageDialog(null,
+            "Import complete.",
+            "Import complete",
+            JOptionPane.INFORMATION_MESSAGE);
+      }
+    }.start();        
+  }
+  
+  /**
+   * Prompt the user for the directory to import the JPEG files from.
+   * 
+   * @return  the directory the user choose, or null if they didn't choose one
+   */
+  private File getDirectory() {
+    JFileChooser directoryChooser = new JFileChooser();
+    directoryChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+    
+    int returnVal = directoryChooser.showDialog(null, "Import");
+    
+    if (returnVal == JFileChooser.APPROVE_OPTION) {
+      return directoryChooser.getSelectedFile();
+    } else {
+      return null;
+    }
+  }
+  
+  /**
+   * Upload the JPEG files in the given directory to the RBNB server.
+   * 
+   * @param directory               the directory to get the files from
+   * @throws FileNotFoundException  if the directory doesn't exist
+   * @throws ParseException         if there is an error parsing the data files
+   * @throws IOException            if there is an error reading the data files
+   * @throws RBNBException          if there is an error communicating with the
+   *                                server
+   */
+  private void importDirectory(File directory) throws FileNotFoundException, ParseException, IOException, RBNBException {   
+    JPEGFileCollectionReader reader = new JPEGFileCollectionReader(directory);
+    int samples = reader.getFileCount();
+    
+    String name = directory.getName();
+    RBNBController rbnb = RBNBController.getInstance();
+    RBNBSource source = new RBNBSource(name, samples,
+        rbnb.getRBNBHostName(), rbnb.getRBNBPortNumber());
+    
+    String channel = "video.jpg";
+    String mime = "image/jpeg";
+    source.addChannel(channel, mime);
+    
+    int currentSample = 0;
+    
+    JPEGFileDataSample sample;
+    while ((sample = reader.readSample()) != null) {
+      String fileName = sample.getFile().getName();
+      progressWindow.setStatus("Importing JPEG file " + fileName);
+      
+      source.putData(channel, sample.getTimestamp(), sample.getData());
+      source.flush();
+      
+      currentSample++;
+      progressWindow.setProgress((float)currentSample/samples);
+    }
+    
+    source.close();
+  }
+}
---
+++ tags/RDV-1.6/src/org/nees/buffalo/rdv/rbnb/MarkerManager.java
@@ -0,0 +1,185 @@
+/*
+ * RDV
+ * Real-time Data Viewer
+ * http://it.nees.org/software/rdv/
+ * 
+ * Copyright (c) 2005-2007 University at Buffalo
+ * Copyright (c) 2005-2007 NEES Cyberinfrastructure Center
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ * $URL$
+ * $Revision$
+ * $Date$
+ * $Author$
+ */
+
+package org.nees.buffalo.rdv.rbnb;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.nees.rbnb.DataTurbine;
+import org.nees.rbnb.marker.EventMarker;
+
+import com.rbnb.sapi.ChannelMap;
+
+/**
+ * A class to manage available event markers and put new markers on the server.
+ * 
+ * @author Jason P. Hanley
+ */
+public class MarkerManager implements DataListener {
+  private String rbnbSourceName = "_Events";
+  private String rbnbChannel = "EventsChannel";
+  
+  private RBNBController rbnbController;
+  
+  private List<EventMarker> markers;
+  
+  private List<EventMarkerListener> markerListeners;
+
+  /**
+   * Create the marker manager and starts listening for new markers from the
+   * metadata manager.
+   * 
+   * @param rbnbController  the rbnb interface
+   */
+  public MarkerManager(RBNBController rbnbController) {
+    super();
+    
+    try {
+      InetAddress addr = InetAddress.getLocalHost();
+      String hostname = addr.getHostName();
+      rbnbSourceName += "@" + hostname;
+    } catch (UnknownHostException e) {}
+    
+    this.rbnbController = rbnbController;
+    
+    markers = new ArrayList<EventMarker>();
+    
+    markerListeners = new ArrayList<EventMarkerListener>();
+    
+    rbnbController.getMetadataManager().addMarkerListener(this);
+  }
+
+  /**
+   * Called when new markers are available.
+   * 
+   * @param channelMap  the channel map containing the marker data.
+   */
+  public void postData(ChannelMap channelMap) {
+    if (channelMap == null) {
+      markers.clear();
+      fireMarkersCleared();
+      return;
+    }
+    
+    if (channelMap.NumberOfChannels() == 0) {
+      return;
+    }
+    
+    for (int i = 0; i < channelMap.NumberOfChannels(); i++) {
+      String channelName = channelMap.GetName(i);
+      int channelIndex = channelMap.GetIndex(channelName);
+      String[] markerData = channelMap.GetDataAsString(channelIndex);
+      for (String markerString : markerData) {
+        EventMarker marker = new EventMarker();
+        try {
+          marker.setFromEventXml(markerString);
+        } catch (Exception e) {
+          continue;
+        }
+        
+        if (!markers.contains(marker)) {
+          markers.add(marker);
+          Collections.sort(markers);
+          
+          fireNewMarker(marker);
+        }
+      }
+    }    
+  }
+  
+  /**
+   * Retruns a list of all event markers.
+   * 
+   * @return  a list of event markers
+   */
+  public List<EventMarker> getMarkers() {
+    return Collections.unmodifiableList(markers);
+  }
+  
+  /**
+   * Puts the event marker on the server.
+   * 
+   * @param eventMarker  the event marker to put
+   * @throws Exception   if the marker could not be sent to the server
+   */
+  public void putMarker(EventMarker eventMarker) throws Exception {    
+    DataTurbine markerSource = new DataTurbine (rbnbSourceName);
+    markerSource.setServerName(rbnbController.getRBNBHostName());
+    markerSource.open();
+    markerSource.putMarker(eventMarker, rbnbChannel);
+    markerSource.closeAndKeep();
+    
+    rbnbController.updateMetadata();
+  }
+  
+  /**
+   * Add a listener for new event markers.
+   * 
+   * @param listener  the event marker listener to add
+   */
+  public void addMarkerListener(EventMarkerListener listener) {
+    markerListeners.add(listener);
+  }
+  
+  /**
+   * Remove a listener for new event markers.
+   * 
+   * @param listener  the event marker listener to remove
+   */
+  public void removeMarkerListener(EventMarkerListener listener) {
+    markerListeners.remove(listener);
+  }
+  
+  /**
+   * Send the new event marker to the registered listeners.
+   * 
+   * @param marker  the new event marker to send
+   */
+  protected void fireNewMarker(EventMarker marker) {
+    for (EventMarkerListener listener : markerListeners) {
+      listener.eventMarkerAdded(marker);
+    }
+  }
+  
+  /**
+   * Send the markers cleared signal to the registered listeners.
+   */
+  protected void fireMarkersCleared() {
+    for (EventMarkerListener listener : markerListeners) {
+      listener.eventMarkersCleared();
+    }
+  }
+}
